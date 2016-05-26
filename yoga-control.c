073fa42eb7a291b879261bc651c8a1787a1a0cdb
khpylon <ken.l.hughes@gmail.com>
2016-05-26 08:37:45 -0700

    Add files via upload
    Changes to work as user-mode client.

--- yoga-control.c
+++ yoga-control.c
@@ -21,92 +21,28 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-
-// constants from Y2P-PM code base
-
-#define EC_SC 0x66
-#define EC_DATA 0x62
-#define IBF 1
-#define OBF 0
-#define EC_SC_READ_CMD 0x80
-#define EC_SC_WRITE_CMD 0x81
-#define EC_SC_SCI_CMD 0x84
-
-// additional constants for use with EC code
-
-#define R_EC_KEYBOARD 0x40
-#define TABLET_MODE 0x20
+#include <pwd.h>
+#include <fcntl.h>
 
 // signals for which we listen
 
 #define KEYTOGGLE  SIGUSR1
 #define KEYLOCK    SIGUSR2
-#define TIMERPOLL  (_NSIG-1)
 
 // lockfile name in /var/run
 
-#define PIDFILE "/var/run/yoga.pid"
 
 // hooks to iio-sensor-proxy on DBus
 
 #define IIOSENSORPROXYBUS  "net.hadess.SensorProxy"
 #define IIOSENSORPROXYPATH "/net/hadess/SensorProxy"
 
+#define KEYBOARDBUS  "net.khpylon.yoga900keyboard"
+#define KEYBOARDPATH "/net/khpylon/yoga900keyboard"
+
 static GDBusProxy *iioSensorProxy;
+static GDBusProxy *keyboard;
 static timer_t timerid;
-
-int ec_init()
-{
-    if (ioperm(EC_DATA, 1, 1) != 0 || ioperm(EC_SC, 1, 1) != 0)
-        return -1;
-    return 0;
-}
-
-uint8_t wait_ec(const uint32_t port, const uint32_t flag, const char value)
-{
-    uint8_t data;
-    int i;
-
-    i = 0;
-    data = inb(port);
-
-    while ( (((data >> flag) & 0x1) != value) && (i++ < 100) )
-    {
-        usleep(1000);
-        data = inb(port);
-    }
-
-    if (i >= 100)
-    {
-        return 0;
-    }
-    return 1;
-}
-
-uint8_t read_ec(const uint32_t port, uint8_t *value)
-{
-
-    if (!wait_ec(EC_SC, IBF, 0)) return 0;
-    outb(EC_SC_READ_CMD, EC_SC);
-    if (!wait_ec(EC_SC, IBF, 0)) return 0;
-    outb(port, EC_DATA);
-    if (!wait_ec(EC_SC, OBF, 1)) return 0;
-    *value = inb(EC_DATA);
-
-    return 1;
-}
-
-uint8_t write_ec(const uint32_t port, const uint8_t value)
-{
-    if (!wait_ec(EC_SC, IBF, 0)) return 0;
-    outb(EC_SC_WRITE_CMD, EC_SC);
-    if (!wait_ec(EC_SC, IBF, 0)) return 0;
-    outb(port, EC_DATA);
-    if (!wait_ec(EC_SC, IBF, 0)) return 0;
-    outb(value, EC_DATA);
-    if (!wait_ec(EC_SC, IBF, 0)) return 0;
-    return 1;
-}
 
 static bool
 forkCommand (char **args)
@@ -117,6 +53,7 @@ forkCommand (char **args)
   {
     if (childPID == 0) // child process
     {
+      setuid(1000);
       execv( args[0], args );
     }
     return false;
@@ -187,24 +124,24 @@ xrandr (char *data)
 static char
 *onboardArgs[] = {"/usr/bin/dbus-send",
                  "--type=method_call",
+                 "--session",
                   "--dest=org.onboard.Onboard",
                   "/org/onboard/Onboard/Keyboard",
                   NULL,
-                 "--system",
                   NULL};
 
 static void
 enableKeyboard (void)
 {
-  onboardArgs[4] = "org.onboard.Onboard.Keyboard.Show";
-  // forkCommand (onboardArgs);
+  onboardArgs[5] = "org.onboard.Onboard.Keyboard.Show";
+  forkCommand (onboardArgs);
 }
 
 static void
 disableKeyboard (void)
 {
-  onboardArgs[4] = "org.onboard.Onboard.Keyboard.Hide";
-  // forkCommand (onboardArgs);
+  onboardArgs[5] = "org.onboard.Onboard.Keyboard.Hide";
+  forkCommand (onboardArgs);
 }
 
 enum States
@@ -294,6 +231,27 @@ stateMachine (enum Events event,
 }
 
 static void
+kproperties_changed (GDBusProxy *proxy,
+		    GVariant   *changed_properties,
+		    GStrv       invalidated_properties,
+		    gpointer    user_data)
+{
+  GVariant *v;
+  GVariantDict dict;
+
+  g_variant_dict_init (&dict, changed_properties);
+
+  if (g_variant_dict_contains (&dict, "LaptopMode")) {
+    v = g_dbus_proxy_get_cached_property (keyboard, 
+                                          "LaptopMode");
+    g_message ("mode %s", g_variant_get_boolean (v) ? 
+               "true" : "false");
+    g_variant_unref (v);
+    stateMachine (KEYBOARD, NULL);
+  }
+}
+
+static void
 properties_changed (GDBusProxy *proxy,
 		    GVariant   *changed_properties,
 		    GStrv       invalidated_properties,
@@ -321,6 +279,38 @@ properties_changed (GDBusProxy *proxy,
 }
 
 static void
+kappear_handler (GDBusConnection *connection,
+	        const gchar     *name,
+	        const gchar     *name_owner,
+	        gpointer         user_data)
+{
+  GError *error = NULL;
+
+  keyboard = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
+                                                  G_DBUS_PROXY_FLAGS_NONE,
+                                                  NULL,
+                                                  KEYBOARDBUS,
+                                                  KEYBOARDPATH,
+                                                  KEYBOARDBUS,
+                                                  NULL, NULL);
+
+  g_signal_connect (G_OBJECT (keyboard), "g-properties-changed",
+                    G_CALLBACK (kproperties_changed), NULL);
+
+  g_assert_no_error (error);
+  g_message ("kappear");
+}
+
+static void
+kvanish_handler (GDBusConnection *connection,
+	        const gchar *name,
+	        gpointer user_data)
+{
+  g_clear_object (&keyboard);
+  g_message ("kvanish");
+}
+
+static void
 appear_handler (GDBusConnection *connection,
 	        const gchar     *name,
 	        const gchar     *name_owner,
@@ -343,6 +333,7 @@ appear_handler (GDBusConnection *connection,
                           NULL, G_DBUS_CALL_FLAGS_NONE, -1,
                           NULL, &error);
   g_assert_no_error (error);
+  g_message ("appear");
 }
 
 static void
@@ -351,6 +342,7 @@ vanish_handler (GDBusConnection *connection,
 	        gpointer user_data)
 {
   g_clear_object (&iioSensorProxy);
+  g_message ("kvanish");
 }
 
 static void
@@ -368,43 +360,37 @@ key_handler (int signo)
   }
 }
 
-static bool
-read_keyboard_status (bool *status)
-{
-  uint8_t tmp1;
-  if (!read_ec (R_EC_KEYBOARD, &tmp1)) return 0;
-  
-  *status = ((tmp1 & TABLET_MODE) == 0);
-  return 1;
-}
+static char *config = "/.config/";
+static char *yogadir = "yoga-control";
+static char *pidfile = "/pid";
 
-static void
-timer_handler (int sig,
-               siginfo_t *si,
-               void *uc)
+static char *
+getPIDFilename (void)
 {
-  if (sig == TIMERPOLL && si->si_value.sival_ptr == &timerid) {
-    bool status;
-    if (read_keyboard_status (&status))
-    {
-      if (keyboardEnabled != status)
-      {
-        g_message ("Caught keyboard state change");
-        stateMachine (KEYBOARD, NULL);
-        keyboardEnabled = status;
-      }
-    }
-  }
+  uid_t uid = getuid ();
+  struct passwd *pwd = getpwuid (uid);
+  int len = strlen (pwd->pw_dir) + strlen (config) +
+      strlen (yogadir) + strlen (pidfile) + 1;
+  char *dirname = malloc (len-1);
+
+  strcpy (dirname, pwd->pw_dir);
+  strcat (dirname, config);
+  int dirfd = open( dirname, O_DIRECTORY );
+  int err = mkdirat( dirfd, yogadir, 0777 );
+  strcat (dirname, yogadir);
+  strcat (dirname, pidfile);
+  return dirname;
 }
 
 // Write our PID value to the lock file
 void
 createPIDFile (void)
 {
-  pid_t pid = getpid ();
-  FILE *f = fopen (PIDFILE, "w");
+  char *filename = getPIDFilename ();
+  FILE *f = fopen (filename, "w");
   fprintf (f, "%d\n", getpid ());
   fclose (f);
+  free (filename) ;
 }
 
 static void
@@ -443,10 +429,12 @@ checkPID (void)
   char exe2[256];
   int len;
   pid_t pid;
+  char *filename = getPIDFilename ();
 
   // Attempt to read PID for a running process.  If the open fails, then
   // assume the process is not running and return 0.
-  FILE *f = fopen (PIDFILE, "r");
+  FILE *f = fopen (filename, "r");
+  free (filename);
   if (f == NULL) return 0;
 
   // If we can't read a valid number from PID file, fail.
@@ -492,38 +480,19 @@ main (int argc,
   // write our PID file into the /var/run directory
   createPIDFile ();
 
-  keyboardEnabled = true;
-
-  // get permissions to read EC memory
-  ec_init();
-
   // handle iio-sensor-proxy PropertyChange events on the DBus
   g_bus_watch_name (G_BUS_TYPE_SYSTEM, IIOSENSORPROXYBUS,
                     G_BUS_NAME_WATCHER_FLAGS_NONE,
                     appear_handler, vanish_handler, NULL, NULL);
 
+  // handle iio-sensor-proxy PropertyChange events on the DBus
+  g_bus_watch_name (G_BUS_TYPE_SYSTEM, KEYBOARDBUS,
+                    G_BUS_NAME_WATCHER_FLAGS_NONE,
+                    kappear_handler, kvanish_handler, NULL, NULL);
+
   // handle key press events using signals
-  signal(KEYTOGGLE, key_handler);
-  signal(KEYLOCK, key_handler);
-
-  // handle checking the EC memory using a timer
-  sa.sa_flags = SA_SIGINFO;
-  sa.sa_sigaction = timer_handler;
-  sigemptyset(&sa.sa_mask);
-  sigaction(TIMERPOLL, &sa, NULL);
-  sigaction(SIGKILL, &sa, NULL);
-
-  sev.sigev_notify = SIGEV_SIGNAL;
-  sev.sigev_signo = TIMERPOLL;
-  sev.sigev_value.sival_ptr = &timerid;
-  timer_create (CLOCK_REALTIME, &sev, &timerid);
-
-  its.it_value.tv_sec = 1;
-  its.it_value.tv_nsec = 0;
-  its.it_interval.tv_sec = its.it_value.tv_sec;
-  its.it_interval.tv_nsec = its.it_value.tv_nsec;
-
-  timer_settime (timerid, 0, &its, NULL);
+  signal (KEYTOGGLE, key_handler);
+  signal (KEYLOCK, key_handler);
 
   // wait in the main loop for events to happen
   g_main_loop_run ( g_main_loop_new (NULL, TRUE) );
